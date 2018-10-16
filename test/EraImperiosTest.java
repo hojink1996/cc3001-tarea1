b5124c2ef2bf1f59167a23777b430e82e23f7115
Hojin Kang <hojink1996@gmail.com>
2018-10-16 00:48:28 -0300

    Agregar test para ataque de cavalry

--- test/EraImperiosTest.java
+++ test/EraImperiosTest.java
@@ -163,6 +163,40 @@ public class EraImperiosTest {
         assertEquals(1196.5, barracks.getHP(), 0.01);
     }
 
+    // Test cavalry attacking
+    @Test public void cavalryAttackTest() {
+        // Attack infantry
+        cavalry.attack(infantry);
+        assertEquals(32, infantry.getHP(), 0.01);
+
+        // Attack archer
+        cavalry.attack(archer);
+        assertEquals(23, archer.getHP(), 0.01);
+
+        // Attack cavalry
+        cavalry.attack(cavalry);
+        assertEquals(142, cavalry.getHP(), 0.01);
+
+        // Attack siege
+        cavalry.attack(siege);
+        assertEquals(65.4, siege.getHP(), 0.01);
+
+        // Attack monk
+        cavalry.attack(monk);
+        assertEquals(0, monk.getHP(), 0.01);
+
+        // Attack villager
+        cavalry.attack(villager);
+        assertEquals(13, villager.getHP(), 0.01);
+
+        // Attack castle
+        cavalry.attack(castle);
+        assertEquals(4797.6, castle.getHP(), 0.01);
+
+        // Attack barracks
+        cavalry.attack(barracks);
+        assertEquals(1194.4, barracks.getHP(), 0.01);
+    }
     // Max HP test
     @Test public void maxHPTest() {
         AbstractAttacker superMonk = new Monk(25, 5000);
