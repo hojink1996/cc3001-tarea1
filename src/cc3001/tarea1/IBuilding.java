963dd08db5c413b885374d0bec5aee4f64f96dd9
Hojin Kang <hojink1996@gmail.com>
2018-10-23 20:04:28 -0300

    Agregar nuevo UML

--- src/cc3001/tarea1/IBuilding.java
+++ src/cc3001/tarea1/IBuilding.java
@@ -2,7 +2,7 @@ package cc3001.tarea1;
 
 /**
  * IBuilding is the interface that represents all buildings in the game.
- * IBuilding extends the IEnte interface.
+ * IBuilding extends the IAttackable interface.
  */
-public interface IBuilding extends IEnte {
+public interface IBuilding extends IAttackable {
 }
